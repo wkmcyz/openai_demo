17c5f61815371bafb56870a0c290ea1900305ca4
wkm <wkmcyz@163.com>
2023-07-25 16:47:12 +0800

    satisfy ubuntu

--- util/proxy.py
+++ util/proxy.py
@@ -2,6 +2,11 @@ import os
 
 
 def set_proxy():
+    print(os.environ)
     PROXY = os.environ['VPN_PROXY']
     os.environ["http_proxy"] = PROXY
     os.environ["https_proxy"] = PROXY
+
+
+if __name__ == '__main__':
+    set_proxy()
